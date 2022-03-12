89f4243ce21d817449fa9a201108d85867b606fa
Dajana <73293960+Dajana11-99@users.noreply.github.com>
2022-03-12 17:38:26 +0100

    feat: show available period on calendar

--- src/frontend/src/views/BoatOwner/BoatCalendar.vue
+++ src/frontend/src/views/BoatOwner/BoatCalendar.vue
@@ -5,29 +5,26 @@
       <a class="navbar-brand"  href="http://localhost:8080/" >
       <img src="../../assets/logoF1.png" alt="" width="194" height="80" >
       </a>
-
-
-
+    
       </div>
-
+    
     </nav>
 
     <ul class="nav justify-content-center" style="background-color: #0b477b;">
 
-      <li @click="home()" class="nav-item">
+      <li @click="myBoats()" class="nav-item">
       <a style="color: white;" class="nav-link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
       <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
-    </svg> MY CABINS</a>
+    </svg> MY BOATS</a>
       </li>
-      <li @click="addNewCabin()" class="nav-item">
+      <li @click="addNewBoat()" class="nav-item">
       <a style="color: white;" class="nav-link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
   <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-</svg> ADD NEW CABIN</a>
+</svg> ADD NEW  BOAT</a>
       </li>
-
-      <li @click="myProfile()" class="nav-item">
+      <li @click="myAccount()" class="nav-item">
       <a style="color: white;" class="nav-link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
 </svg> MY ACCOUNT</a>
@@ -39,20 +36,21 @@
 </svg> UPCOMING RESERVATIONS</a>
       </li>
        <li class="nav-item">
-      <a style="color: white;" @click="requests()" class="nav-link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+      <a style="color: white;"  class="nav-link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
   <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
 </svg> WRITE A REVIEW</a>
       </li>
 
       <li class="nav-item">
-      <a style="color: white;" @click="requests()" class="nav-link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-line" viewBox="0 0 16 16">
+      <a style="color: white;"  class="nav-link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-line" viewBox="0 0 16 16">
   <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z"/>
 </svg> STATISTCS</a>
       </li>
-
+       
      </ul>
-    
 
+
+     
          <div class="card bg-light text-light" style="margin: 5%">
     <div class="content">
       <FullCalendar :options="calendarOptions"    @select="handleSelect" class="calendar"  />
@@ -159,15 +157,16 @@
     </div>
 </vue-modality>
     </div>
+     
+
 
   </div>
 
 </template>
 
-
-
 <script>
-import { ref} from "vue";
+  import axios from "axios";
+  import { ref} from "vue";
 import "@fullcalendar/core/vdom"; // solves problem with Vite
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -179,14 +178,13 @@ import dayjs from 'dayjs';
 import '@shapla/vue-modal/dist/style.css';
 import VueModality from 'vue-modality-v3'
 
-import axios from "axios";
+
    export default{
-     components: {
+    components: {
     FullCalendar,
     Datepicker,
     VueModality
-  },
-   setup() {
+  },  setup() {
     const startDate = ref();
     const endDate = ref();
     
@@ -199,14 +197,12 @@ import axios from "axios";
       openMyModal,
       startDate,
       endDate,
-     
-    
     };
-  },
-
+    
+    },
      data(){
        return{
-              closeModal: false,
+                closeModal: false,
       disabledPickers: false,
       selectDisabled: false,
       currentEvent: "",
@@ -232,41 +228,56 @@ import axios from "axios";
         events: [],
       },
          email: '',
-          user:{
-           username: ''
-         },
-         cabinName: '',
-          cabinDto: {
-              id: null,
-              name: '',
-              description: '',
-              numOfRooms: 1,
-              bedsPerRoom: 1,
-              rules: '',
-              price: 1.0,
-              addressDto: {
+         boatDto: {
+
+         id: null,
+         ownersUsername: '',
+         name: '',
+         type: '',
+         length: '',
+         engineCode: '',
+         enginePower: '',
+         maxSpeed: '',
+         navigationEquipment: '',
+         addressDto: {
                   longitude: 0.0,
                   latitude: 0.0,
                   country: '',
                   city: '',    
                   streetAndNum: ''
-              },
-             additionalServices: [{
-                 id: null,
-                 name: '',
-                 price: 0.0
-
-             }],
-             rating: 0.0,
-             images: [{
-                 id: null,
-                 url: '',
-                 cabin: ''
-
-             }],
-             ownerUsername: ''
          },
-       availableCabinPeriod: [{
+         description: '',
+         images:[{
+                  id: null,
+                  url: '',
+                  cabin: ''
+         }],
+         maxPeople: 1, 
+         rules: '',
+         fishingEquipment: '',
+         price: 1.0,
+         additionalServices: [{
+                  id: null,
+                  name: '',
+                  price: 0.0  
+         }],
+         cancelingCondition: '',
+         rating: '', 
+         },
+         prices: '',
+           names: '',
+           idx: 0,
+           tableHidden: true,
+           selectedFile: null,
+           imagesSelected: false,
+           imagesSelectedEvent: null,
+           additionalServicesAdded: false,
+           loader: null,
+           user:{
+            username: ''
+           },
+           boatName: '',
+              availableBoatPeriod: [{
                  id: null,
                 startDate: null,
                 endDate: null,
@@ -275,31 +286,54 @@ import axios from "axios";
             }],
             start: null,
             end: null,
-            cabinId: null,
-        
+            boatId: null,
 
        }
+
      },
      mounted() {
        this.email = this.$route.params.email
-       this.cabinName= this.$route.params.cabinName
-       this.availableCabinPeriod.username=this.email
-       this.getCabin()
-      
-
-
-
+       this.boatName= this.$route.params.boatName
+       this.getBoat()
      },
      methods: {
-         getCabinsAvailablePeriod: function(){
-               axios.post("http://localhost:8081/cabinsPeriod/getAvailablePeriod",this.cabinDto,{
+       myAccount: function(){
+          this.$router.push('/editProfile/'+'boatOwner/'+ this.email);
+       },
+       addNewBoat: function(){
+          this.$router.push('/addNewBoat/'+ this.email);
+       },
+       myBoats: function(){
+          this.$router.push('/boatOwnerHome/'+ this.email);
+       },
+       editProfile: function(){
+         this.$router.push('/editBoatProfile/'+ this.email+'/'+this.boatName);
+       },
+       getBoat: function(){
+                  this.boatDto.name=this.boatName
+                  this.boatDto.ownersUsername=this.email
+            axios.post("http://localhost:8081/boats/findByName",this.boatDto,{
+                  headers: {
+                  "Access-Control-Allow-Origin": process.env.VUE_APP_URL,
+                  "Authorization": "Bearer " + localStorage.jwt ,
+                  }
+                  })
+                  .then(response => {
+                        this.boatDto=response.data
+                        this.boatId = this.boatDto.id
+                        this.getBoatsAvailablePeriod();
+                  })
+
+       },
+         getBoatsAvailablePeriod: function(){
+               axios.post("http://localhost:8081/boatsPeriod/getAvailablePeriod",this.boatDto,{
             headers: {
             "Access-Control-Allow-Origin": process.env.VUE_APP_URL,
             "Authorization": "Bearer " + localStorage.jwt ,
             }
              })
                .then(response => {
-                  this.availableCabinPeriod=response.data
+                  this.availableBoatPeriod=response.data
                      for( let newData of response.data ){
                                 var start=newData.startDate
                                 var end=newData.endDate
@@ -313,58 +347,21 @@ import axios from "axios";
 
 
          },
-       myProfile: function(){
-         this.$router.push('/editProfile/'+'cabinOwner/'+ this.email);
-       },
-       addNewCabin: function(){
-         this.$router.push('/addNewCabin/'+ this.email);
 
-       },
-       home: function(){
-         this.$router.push('/cabinOwnerHome/'+ this.email);
-
-       },
-       editProfile: function(){
-        this.$router.push('/editCabinProfile/'+ this.email+'/'+this.cabinName);
-       },
-       getCabin: function(){
-             this.cabinDto.name=this.cabinName
-            
-             axios.post("http://localhost:8081/cabins/findByName",this.cabinDto,{
-            headers: {
-            "Access-Control-Allow-Origin": process.env.VUE_APP_URL,
-            "Authorization": "Bearer " + localStorage.jwt ,
-            }
-             })
-               .then(response => {
-                        this.cabinDto=response.data
-                         this.cabinId=this.cabinDto.id
-                         this.getCabinsAvailablePeriod();
-                      
-                      
-              })
-
-       },
         formatDate(formatDate) {
-            console.log("preeformat"+formatDate)
             const date = dayjs(formatDate);
            return date.format('YYYY-MM-DDTHH:mm:ss');
         },setPeriod: function(){
-          console.log("starttt"+this.start)
-           console.log("endd"+this.end)
-         
-            this.availableCabinPeriod[0]=({
+            this.availableBoatPeriod[0]=({
               startDate:  this.formatDate(this.start),
               endDate:  this.formatDate(this.end),
               username: this.email,
-              propertyId: this.cabinId
-           
-              
+              propertyId: this.boatId
              })
               this.start='',
               this.end=''
     
-          axios.post("http://localhost:8081/cabinsPeriod/setAvailableCabinsPeriod",this.availableCabinPeriod,{
+          axios.post("http://localhost:8081/boatsPeriod/setAvailableBoatsPeriod",this.availableBoatPeriod,{
             headers: {
             "Access-Control-Allow-Origin": process.env.VUE_APP_URL,
             "Authorization": "Bearer " + localStorage.jwt ,
@@ -391,7 +388,6 @@ import axios from "axios";
     setDate: function(newDate){
       
       var date= new Date()
-      console.log("VREDNOSTII"+newDate.toString())
           var splits =newDate.toString().split(",")
           date.setDate( splits[1],splits[2], splits[0])
           console.log("0-"+splits[0])
@@ -404,6 +400,10 @@ import axios from "axios";
     },
 
     
+    
+       
+       
+      
     }
   }
 
@@ -417,6 +417,7 @@ import axios from "axios";
   text-align: center;
   color: #2c3e50;
 }
+
 #nav {
   padding: 30px;
 }
@@ -424,31 +425,13 @@ import axios from "axios";
   width: 47%;
   background-image:  url("../../assets/IMG_3872.jpeg"); 
 }
+
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
+
 #nav a.router-link-exact-active {
   color: #42b983;
-}
-.element {
-  margin-bottom: 2rem;
-}
-.content {
-  display: flex;
-  justify-content: space-between;
-}
-.calendar {
-  width: 70%;
-  color: #1a252f;
-  
-  
-}
-.info {
-  background: #1a252f;
-  width: 28%;
-  border-radius: 5px;
-  border: 1px solid white;
-  padding: 1rem;
 }
 </style>
