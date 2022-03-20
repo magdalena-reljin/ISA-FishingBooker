122caed993c9687c8357b29f110b5e3d03c38497
Magdalena Reljin <reljin.ra82.2018@uns.ac.rs>
2022-03-20 15:50:53 +0100

    refactor: removed sonar analysis for tests

--- src/test/java/rs/ac/uns/ftn/isa/fisherman/JpaExampleApplicationTests.java
+++ src/test/java/rs/ac/uns/ftn/isa/fisherman/JpaExampleApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JpaExampleApplicationTests {
+public class JpaExampleApplicationTests //NOSONAR
+{
 
 	@Test
 	public void contextLoads() {
