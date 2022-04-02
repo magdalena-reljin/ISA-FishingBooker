ecab6456e701813bf7de74e3814ef6423db147bb
Magdalena Reljin <reljin.ra82.2018@uns.ac.rs>
2022-04-02 11:42:36 +0200

    feat: search boat reservation history

--- src/main/java/rs/ac/uns/ftn/isa/fisherman/repository/BoatReservationRepository.java
+++ src/main/java/rs/ac/uns/ftn/isa/fisherman/repository/BoatReservationRepository.java
@@ -21,13 +21,13 @@ public interface BoatReservationRepository extends JpaRepository<BoatReservation
     @Query(value="SELECT CASE WHEN  COUNT(res) > 0 THEN true ELSE false END FROM boat_reservation res join boat b on res.boat_id=b.id where b.users_id=:users_id and needs_captain_service=true and ((:startDate between start_date and end_date) or (:endDate  between start_date and end_date) or (start_date  between :startDate and :endDate) or (end_date  between :startDate and :endDate)) ",nativeQuery = true)
     boolean ownerIsNotAvailable(@Param("users_id")Long ownerId, @Param("startDate") LocalDateTime start, @Param("endDate") LocalDateTime end);
 
-    @Query(value="SELECT * FROM boat_reservation res join boat b on res.boat_id=b.id where b.users_id=:users_id ",nativeQuery = true)
-    Set<BoatReservation> findReservationsByOwnerId(@Param("users_id")Long id);
+    @Query(value="SELECT * FROM boat_reservation res join boat b on res.boat_id=b.id where b.users_id=:users_id and (:currentDate <= end_date) ",nativeQuery = true)
+    Set<BoatReservation> findReservationsByOwnerId(@Param("users_id")Long id, @Param("currentDate")LocalDateTime currentDate);
 
     @Query(value="SELECT CASE WHEN  COUNT(c) > 0 THEN true ELSE false END FROM boat_reservation c where boat_id=:boat_id and (:currentDate <= end_date) ",nativeQuery = true)
     boolean futureReservationsExist(@Param("currentDate")LocalDateTime currentDate,@Param("boat_id") Long boatId);
 
-    @Query(value="SELECT * FROM boat_reservation where boat_id=:boat_id and (:currentDate > end_date) ",nativeQuery = true)
-    Set<BoatReservation> getPastReservations(@Param("boat_id")Long boatId, @Param("currentDate")LocalDateTime currentDate);
+    @Query(value="SELECT * FROM boat_reservation res join boat b on res.boat_id=b.id  where b.users_id=:users_id and (:currentDate > end_date) ",nativeQuery = true)
+    Set<BoatReservation> getPastReservations(@Param("users_id")Long boatId, @Param("currentDate")LocalDateTime currentDate);
 
 }
--- src/main/java/rs/ac/uns/ftn/isa/fisherman/repository/QuickReservationBoatRepository.java
+++ src/main/java/rs/ac/uns/ftn/isa/fisherman/repository/QuickReservationBoatRepository.java
@@ -2,6 +2,7 @@ package rs.ac.uns.ftn.isa.fisherman.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import rs.ac.uns.ftn.isa.fisherman.model.BoatReservation;
 import rs.ac.uns.ftn.isa.fisherman.model.QuickReservationBoat;
 
 import java.time.LocalDateTime;
@@ -17,9 +18,13 @@ public interface QuickReservationBoatRepository extends JpaRepository<QuickReser
     @Query(value="SELECT CASE WHEN  COUNT(res) > 0 THEN true ELSE false END FROM quick_reservation_boat res join boat b on res.boat_id=b.id where b.users_id=:users_id and res.needs_captain_services=true and ((:startDate between start_date and end_date) or (:endDate  between start_date and end_date) or (start_date  between :startDate and :endDate) or (end_date  between :startDate and :endDate)) ",nativeQuery = true)
     boolean ownerIsNotAvailable(@Param("users_id")Long ownerId, @Param("startDate") LocalDateTime start, @Param("endDate") LocalDateTime end);
 
-    @Query(value="SELECT * FROM quick_reservation_boat res join boat b on res.boat_id=b.id where b.users_id=:users_id ",nativeQuery = true)
-    Set<QuickReservationBoat> findReservationsByOwnerId(@Param("users_id")Long ownerId);
+    @Query(value="SELECT * FROM quick_reservation_boat res join boat b on res.boat_id=b.id where b.users_id=:users_id and (:currentDate <= end_date)",nativeQuery = true)
+    Set<QuickReservationBoat> findReservationsByOwnerId(@Param("users_id")Long ownerId, @Param("currentDate")LocalDateTime currentDate);
 
     @Query(value="SELECT CASE WHEN  COUNT(c) > 0 THEN true ELSE false END FROM quick_reservation_boat c where boat_id=:boat_id and (:currentDate <= end_date) ",nativeQuery = true)
     boolean futureQuickReservationsExist(@Param("currentDate")LocalDateTime currentDate,@Param("boat_id") Long boatId);
+
+    @Query(value="SELECT * FROM quick_reservation_boat res join boat b on res.boat_id=b.id where b.users_id=:users_id and (:currentDate > end_date) ",nativeQuery = true)
+    Set<QuickReservationBoat> getPastReservations(@Param("users_id")Long id, @Param("currentDate")LocalDateTime currentDate);
+
 }
