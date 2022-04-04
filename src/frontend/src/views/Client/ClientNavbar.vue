9382306c0a8953a68c9286ec9f8a32cb2d901725
Aleksa Stojić <aleksa.stojic19@uns.ac.rs>
2022-04-04 18:43:06 +0200

    feat: connected client profile to existing component

--- src/frontend/src/views/Client/ClientNavbar.vue
+++ src/frontend/src/views/Client/ClientNavbar.vue
@@ -53,7 +53,7 @@
 </svg> COMPLAINTS</a>
       </li>
 
-<li class="nav-item">
+<li class="nav-item" @click="myProfile()">
       <a style="color: white;"  class="nav-link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
 </svg> MY ACCOUNT</a>
@@ -86,7 +86,10 @@
        },
        reservations: function(){
          this.$router.push('/reservations/'+ this.email);
-       },      
+       },
+       myProfile: function () {
+        this.$router.push("/editProfile/" + "client/" + this.email);
+    },      
     }
   }
 
