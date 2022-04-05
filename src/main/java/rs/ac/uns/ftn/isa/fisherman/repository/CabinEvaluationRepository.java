6aef5094fdaa54b75e72359874629c264e943f4e
Aleksa Stojić <73871747+stojic19@users.noreply.github.com>
2022-04-05 15:17:45 +0200

    feat: upgrading grade after client evaluation (#119)
    * feat: added sending activation link to mail
    
    * feat: added method for updating cabin grade

--- src/main/java/rs/ac/uns/ftn/isa/fisherman/repository/CabinEvaluationRepository.java
+++ src/main/java/rs/ac/uns/ftn/isa/fisherman/repository/CabinEvaluationRepository.java
@@ -12,6 +12,6 @@ public interface CabinEvaluationRepository extends JpaRepository<CabinEvaluation
     @Query(value="SELECT CASE WHEN  COUNT(c) > 0 THEN true ELSE false END FROM evaluations c where cabin_reservation_id=:cabin_reservation_id",nativeQuery = true)
     boolean reservationHasEvaluation(@Param("cabin_reservation_id")Long cabin_reservation_id);
 
-    @Query(value="SELECT grade  FROM cabin_evaluation c where cabin_reservation_id in :cabin_reservation_ids and approved=true",nativeQuery = true)
+    @Query(value="SELECT grade  FROM evaluations c where cabin_reservation_id in :cabin_reservation_ids and approved=true",nativeQuery = true)
     Set<Double> getAllApprovedCabinEvaluationsByCabinReservationIds(@Param("cabin_reservation_ids") Set<Integer> cabin_reservation_ids);
 }
