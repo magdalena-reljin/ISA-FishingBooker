dd43871ad472190b8594c7cf7515e5466949ddd9
Aleksa Stojić <aleksa.stojic19@uns.ac.rs>
2022-05-30 18:54:31 +0200

    refactor: existing adventure subscription service test

--- src/test/java/rs/ac/uns/ftn/isa/fisherman/service/AdventureSubscriptionServiceTest.java
+++ src/test/java/rs/ac/uns/ftn/isa/fisherman/service/AdventureSubscriptionServiceTest.java
@@ -7,6 +7,8 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import rs.ac.uns.ftn.isa.fisherman.model.Adventure;
+import rs.ac.uns.ftn.isa.fisherman.model.AdventureSubscription;
+import rs.ac.uns.ftn.isa.fisherman.model.Client;
 import rs.ac.uns.ftn.isa.fisherman.repository.AdventureSubscriptionRepository;
 import rs.ac.uns.ftn.isa.fisherman.service.impl.AdventureSubscriptionServiceImpl;
 
@@ -22,19 +24,29 @@ import static org.mockito.Mockito.when;
 public class AdventureSubscriptionServiceTest {
 
     @Mock
-    private AdventureSubscriptionRepository adventureSubscriptionRepositoryMock;
+    private AdventureSubscriptionRepository adventureSubscriptionRepository;
+    @Mock
+    private ClientService clientService;
     @InjectMocks
     private AdventureSubscriptionServiceImpl adventureSubscriptionService;
 
     @Test
-    public void testFindAdventureSubscribers(){
-        Adventure adventure = new Adventure();
-        adventure.setId(1L);
+    public void testFindSubscriptionsByClientUsername(){
+        Client client1 = new Client(1L, "testUser");
+        Client client2 = new Client(2L, "testUser2nd");
+        Client client3 = new Client(3L, "testUser3rd");
 
-        when(adventureSubscriptionRepositoryMock.findAdventureSubscribers(1L)).thenReturn(new HashSet<>(Arrays.asList("testUser1","testUser2")));
+        when(adventureSubscriptionRepository.findAll()).thenReturn(Arrays.asList(
+                new AdventureSubscription(1L, client1, new Adventure()),
+                new AdventureSubscription(2L, client2, new Adventure()),
+                new AdventureSubscription(3L, client1, new Adventure())
+        ));
+        when(clientService.findByUsername("testUser")).thenReturn(client1);
+        when(clientService.findByUsername("testUser2nd")).thenReturn(client2);
+        when(clientService.findByUsername("testUser3rd")).thenReturn(client3);
 
-        Set<String> subscribers = adventureSubscriptionService.findAdventureSubscribers(1L);
-
-        assertThat(subscribers).hasSize(2);
+        assertThat(adventureSubscriptionService.findSubscriptionsByClientUsername("testUser")).hasSize(2);
+        assertThat(adventureSubscriptionService.findSubscriptionsByClientUsername("testUser2nd")).hasSize(1);
+        assertThat(adventureSubscriptionService.findSubscriptionsByClientUsername("testUser3rd")).hasSize(0);
     }
 }
